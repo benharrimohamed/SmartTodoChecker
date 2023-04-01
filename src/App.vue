0192082c9ee86b64a42ba0cc4c272670f683562e
unknown <benharri0mohammed@gmail.com>
2023-04-01 19:21:38 +0000

    layouts and spacing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <ArticleCard/>
+   <ArticleCard/>
+   <ArticleCard/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ArticleCard from './components/ArticleCard.vue'
+import ProfileCard from './components/ProfileCard.vue'
 
 export default {
+  components: { ArticleCard,ProfileCard },
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    ProfileCard</script>
---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="bg-white rounded-lg shadow-lg p-6 mt-6 mx-6 w-1/2 sm:mx-auto">
+    <div class="p-4">
+      <div class="sm:mx-auto uppercase tracking-wide text-sm text-green-500 font-medium">
+        Vue
+      </div>
+      <a
+        href="#"
+        class="mt-6 text-lg tracking-tight font-medium text-black hover:underline"
+        >Building Vue 3 Components With Tailwind CSS
+      </a>
+      <p class="mt-2 text-sm text-slate-500">
+        Learn how to create visually stunning Vue components using the popular
+        utility-first CSS framework Tailwind.
+      </p>
+    </div>
+  </div>
+</template>
